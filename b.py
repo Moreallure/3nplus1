cf992f3a209d25301c0790c4838d8c5ac9d0d60f
Moreallure <111924754+Moreallure@users.noreply.github.com>
2024-05-04 20:02:44 -0500

    Update b.py

--- b.py
+++ b.py
@@ -8,10 +8,10 @@ while True:
 
     if n % 2 == 0:
         n = n / 2
+        steps += 1
     else:
-        n = n * 3 + 1
-
-    steps += 1
+        n = (n * 3 + 1) / 2
+        steps += 2
 
     if n == 1:
         print("Number: " + str(num) + " | Steps: " + str(steps))
